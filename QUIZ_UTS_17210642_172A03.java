2ac289e24fe719bcbed4ce5943ffba218aef1feb
fzltf <92301092+fzltf@users.noreply.github.com>
2023-10-18 20:34:43 +0700

    Add files via upload

---
+++ QUIZ_UTS_17210642_172A03.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.quiz_uts_17210642_172a03;
+/**
+ *
+ * @author Fauza
+ */
+public class QUIZ_UTS_17210642_172A03 {
+    public static void main(String[] args) {
+       JapanFood jf = new JapanFood();
+        jf.setVisible(true);
+        jf.setTitle("Form Pemesanan");
+        jf.pack();
+        jf.setLocationRelativeTo(null);
+        jf.setDefaultCloseOperation(JapanFood.EXIT_ON_CLOSE);
+    }
+  
+}
